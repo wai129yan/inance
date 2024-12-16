88ac1910a4f3c7ab7efe84143fa8e45580eab059
waiyan.msc.yu12@gmail.com <waiyan.msc.yu.12@gmail.com>
2024-12-17 00:27:26 +0900

    profile-edit-button

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+unset($_SESSION['name']);
+unset($_SESSION['career']);
+unset($_SESSION['t_id']);
+unset($_SESSION['user_id']);
 session_destroy();
 header("Location: index.php");
 ?>
